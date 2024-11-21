6e8ee19dd594a84ec6243516decdd4d3d3a9b263
DhimasAdityaP <dimasadit0105@gmail.com>
2024-11-21 19:55:20 +0700

    Commit 9 function NavigationControl dan mengatur nav controller serta membuat enum class Halaman

--- app/src/main/java/com/example/pam7_118/NavigationControl.kt
+++ app/src/main/java/com/example/pam7_118/NavigationControl.kt
@@ -1,2 +1,62 @@
 package com.example.pam7_118
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pam7_118.model.JenisKelamin.JenisK
+import com.example.pam7_118.ui.view.FormulirView
+import com.example.pam7_118.ui.view.TampilDataView
+import com.example.pam7_118.viewmodel.SiswaViewModel
+
+
+
+enum class Halaman {
+    FORMULIR,
+    TAMPILDATA
+}
+
+@Composable
+fun NavigationControl(
+    modifier : Modifier = Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    NavHost(
+        navController = navHost,
+        startDestination = Halaman.FORMULIR.name
+    ) {
+        composable(
+            route = Halaman.FORMULIR.name
+        ) {
+            val konteks = LocalContext.current
+            FormulirView(
+                listJk = JenisK.map { id ->
+                    konteks.getString(id)
+                },
+                onSubmitClicked = {
+                    viewModel.saveDataSiswa(it)
+                    navHost.navigate(Halaman.TAMPILDATA.name)
+                },
+                modifier = modifier
+            )
+        }
+        composable(route = Halaman.TAMPILDATA.name) {
+            TampilDataView(
+                uiState = uiState,
+                onBackButton = {
+                    navHost.popBackStack()
+                }
+            )
+        }
+    }
+}
