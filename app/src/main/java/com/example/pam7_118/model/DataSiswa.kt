5ebb25ef6699ce24c69b2ffec52ecafecd424467
DhimasAdityaP <dimasadit0105@gmail.com>
2024-11-21 19:41:57 +0700

    Commit 2 pembuatan data class DataSiswa

--- app/src/main/java/com/example/pam7_118/model/DataSiswa.kt
+++ app/src/main/java/com/example/pam7_118/model/DataSiswa.kt
@@ -1,3 +1,10 @@
 package com.example.pam7_118.model
 
+data class DataSiswa(
+    val nama: String= "",
+    val alamat: String= "",
+    val gender: String= "",
+    val email: String="",
+    val notelepon: String=""
 
+)
