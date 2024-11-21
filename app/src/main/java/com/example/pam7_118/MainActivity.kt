40aa0566e66cc3b5e98776fe0070542c80c52e1f
DhimasAdityaP <dimasadit0105@gmail.com>
2024-11-21 19:56:19 +0700

    Commit 10 mengatur class MainActivity nya

--- app/src/main/java/com/example/pam7_118/MainActivity.kt
+++ app/src/main/java/com/example/pam7_118/MainActivity.kt
@@ -1,2 +1,46 @@
 package com.example.pam7_118
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pam7_118.ui.theme.PAM7_118Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PAM7_118Theme {
+                Scaffold(modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    NavigationControl(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PAM7_118Theme {
+        Greeting("Android")
+    }
+}
